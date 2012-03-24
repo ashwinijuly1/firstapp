72bb03a1df1121f630cd2c2ea54bec10c52657e8
excel <excel@excel.com>
2012-03-24 19:02:13 +0530

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Mar23::Application.config.secret_token = '7c2d33e9d0f3955ff735d4df40bcd41018d2ab1e26946d8f4203b359c544394fa5481951ac342d3bae597c960346d5702a12185c9b9d9f1d81f718436177bf37'
